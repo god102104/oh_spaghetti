d5755c94aa418e9474150a3486b49ac6a583d1bc
acrord <acrord12@gmail.com>
2021-07-07 04:45:58 +0900

    socket 통신마무리 & 에러코드 수정 & 명령어 분기설정

--- orange/client_socket.py
+++ orange/client_socket.py
@@ -1,38 +1,38 @@
 from socket import *
 import os
 import subprocess
-
+import sys
+import time
+sys.path.append('..')
+import user_setting as usr
 flag = False
-cp = 1
-pat = "0\n"
-flag = False
+ADDR = (usr.Mobile, 5050)
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect(ADDR)
+past = "0\n"
+print("connect")
 while True:
-  ADDR = ('192.168.241.26',5050)
-  clientSocket = socket(AF_INET, SOCK_STREAM)
-  clientSocket.connect(ADDR)
-  print("connect")
-  
   data = clientSocket.recv(1024)
   data = data.decode()
   print(data)
-  print(cp)
-  if flag and past != data:
-   print(cp.pid)
-   os.system('sudo kill '+ str(cp.pid))
-   print(cp.pid)
+  if data =="1\n" and past == data:
    continue
   if data=="1\n":
    flag = True
    print("Find")
-   #cp1 = subprocess.Popen('sudo mjpg_streamer -i "input_uvc.so" -o "output_http.so -p 8081 -w /usr/local/share/mjpg-streamer/www/"', shell = True)
-   cp = subprocess.Popen("python3 ~/oh_spaghetti/orange/car.py", shell = True)
+   os.system("sudo fuser -k -n tcp 4000")
+   time.sleep(1)
+   subprocess.Popen("python3 ~/oh_spaghetti/orange/car.py", shell = True)
   elif data == "2\n":
    flag = True
-   cp = subprocess.Popen("python3 ~/oh_spaghetti/orange/bowl.py", shell = True) 
-   print("check")
+   os.system("sudo fuser -k -n tcp 7000")
+   time.sleep(1)
+   subprocess.Popen("python3 ~/oh_spaghetti/orange/bowl.py", shell = True) 
+   print("Check")
+   flag = False
   elif data == "3\n":
-   flag =True
-   print("aircon")
+   flag = True
+   print("Aircon")
+   flag = False
   past = data
-  clientSocket.close()
 
